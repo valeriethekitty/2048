3b8a4c37562a93b041fdef9b8309ec45298db090
Valerie So <valerieso@gmail.com>
2024-06-13 02:25:14 +0100

    Add down, left, and right movement

--- src/2048.js
+++ src/2048.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { jsxDEV } from 'react/jsx-dev-runtime';
 
 const UP = 0; // create constants for easier reading
 const DOWN = 1;
@@ -15,7 +16,7 @@ function getRandomOpenSquare(squares) { // get a random open square to add the n
 
     }
   }
-  return [3, 0];
+  return [3, 0]; // placeholder
 }
 
 function newValue() { // return randomly either a 2 or 4, to be added when you move
@@ -42,20 +43,40 @@ function move(squares, direction) { // return the new squares to move to
     }
   }
   else if (direction == DOWN) { // for down, it needs to count rows backward for the priority
-    /*for (let i = squares.length - 1; i >= 0; i--) {
-      if (squares[i] != null) {
-        let newPos = newPosition(nextSquares, i, direction);
-        if (newPos) {
-          nextSquares[newPos] = squares[i];
+    for (let i = 3; i >= 0; i--) {
+      for (let j = 0; j < 4; j++) {
+        if (squares[i][j] != null) {
+          let x = newPosition(nextSquares, i, j, direction);
+          if (x != null) {
+            nextSquares[x][j] = squares[i][j];
+          }
         }
       }
-    }*/
+    }
   }
   else if (direction == LEFT) { // for left, it needs to count columns forward for the priority
-
+    for (let j = 0; j < 4; j++) {
+      for (let i = 0; i < 4; i++) {
+        if (squares[i][j] != null) {
+          let y = newPosition(nextSquares, i, j, direction);
+          if (y != null) {
+            nextSquares[i][y] = squares[i][j];
+          }
+        }
+      }
+    }
   }
   else if (direction == RIGHT) { // for right, it needs to count columns backward for the priority
-
+    for (let j = 3; j >= 0; j--) {
+      for (let i = 0; i < 4; i++) {
+        if (squares[i][j] != null) {
+          let y = newPosition(nextSquares, i, j, direction);
+          if (y != null) {
+            nextSquares[i][y] = squares[i][j];
+          }
+        }
+      }
+    }
   }
   return nextSquares;
 }
@@ -99,21 +120,120 @@ function newPosition(nextSquares, i, j, direction) { // return new position of s
       }
     }
   }
-  /*else if (direction == DOWN) { // down priority { {3, 0}, {3, 1}, {3, 2}, {3, 3} }, { {2, 0}, {2, 1}, {2, 2}, {2, 3} }, { {1, 0}, {1, 1}, {1, 2}, {1, 3} }, { {0, 0}, {0, 1}, {0, 2}, {0, 3} }
-    if (i + 4 > 15) { // if i is bottom row, it does not move 
-      return i;  
+  else if (direction == DOWN) { // down priority { {3, 0}, {3, 1}, {3, 2}, {3, 3} }, { {2, 0}, {2, 1}, {2, 2}, {2, 3} }, { {1, 0}, {1, 1}, {1, 2}, {1, 3} }, { {0, 0}, {0, 1}, {0, 2}, {0, 3} }
+    if (i == 3) { // if i, j is bottom row, it does not move
+      return i;
+    }
+    if (i == 2) { // check the next higher row
+      if (nextSquares[i + 1][j] == null) { // if the lower square is null, it moves there
+        return i + 1; // j stays the same so just return the new i value
+      }
+      else { // otherwise, it does not move
+        return i;
+      }
+    }
+    if (i == 1) { // check the next higher row 
+      if (nextSquares[i + 2][j] == null) { // if the lowest square is null, it moves there
+        return i + 2;
+      }
+      if (nextSquares[i + 1][j] == null) { // if the next lowest square is null, it moves there
+        return i + 1;
+      }
+      else { // otherwise, it does not move
+        return i;
+      }
+    }
+    if (i == 0) { // check the next row 
+      if (nextSquares[i + 3][j] == null) { // if the lowest square is null, it moves there
+        return i + 3;
+      }
+      if (nextSquares[i + 2][j] == null) { // if the next lowest square is null, it moves there
+        return i + 2;
+      }
+      if (nextSquares[i + 1][j] == null) { // if the next lowest square is null, it moves there
+        return i + 1;
+      }
+      else { // otherwise, it does not move
+        return i;
+      }
     }
   }
   else if (direction == LEFT) { // left priority { {0, 0}, {1, 0}, {2, 0}, {3, 0} }, { {0, 1}, {1, 1}, {2, 1}, {3, 1} }, { {0, 2}, {1, 2}, {2, 2}, {3, 2} }, { {0, 3}, {1, 3}, {2, 3}, {3, 3} }
-    if (i % 4 == 0) { // if i is left column, it does not move 
-      return i;
+    if (j == 0) { // if i, j is leftmost column, it does not move
+      return j;
+    }
+    if (j == 1) { // check the next column
+      if (nextSquares[i][j - 1] == null) { // if the square is null, it moves there
+        return j - 1; // i stays the same so just return the new j value
+      }
+      else { // otherwise, it does not move
+        return j;
+      }
+    }
+    if (j == 2) { // check the next column
+      if (nextSquares[i][j - 2] == null) { // if the leftmost square is null, it moves there
+        return j - 2;
+      }
+      if (nextSquares[i][j - 1] == null) { // if the next square is null, it moves there
+        return j - 1;
+      }
+      else { // otherwise, it does not move
+        return j;
+      }
+    }
+    if (j == 3) { // check the next row 
+      if (nextSquares[i][j - 3] == null) { // if the leftmost square is null, it moves there
+        return j - 3;
+      }
+      if (nextSquares[i][j - 2] == null) { // if the next square is null, it moves there
+        return j - 2;
+      }
+      if (nextSquares[i][j - 1] == null) { // if the next square is null, it moves there
+        return j - 1;
+      }
+      else { // otherwise, it does not move
+        return j;
+      }
     }
   }
   else if (direction == RIGHT) { // right priority { {0, 3}, {1, 3}, {2, 3}, {3, 3} }, { {0, 2}, {1, 2}, {2, 2}, {3, 2} }, { {0, 1}, {1, 1}, {2, 1}, {3, 1} }, { {0, 0}, {1, 0}, {2, 0}, {3, 0} }
-    if (i % 4 == 3) { // if i is right column, it does not move
-      return i;
+    if (j == 3) { // if i, j is rightmost column, it does not move
+      return j;
     }
-  }*/
+    if (j == 2) { // check the next column
+      if (nextSquares[i][j + 1] == null) { // if the square is null, it moves there
+        return j + 1; // i stays the same so just return the new j value
+      }
+      else { // otherwise, it does not move
+        return j;
+      }
+    }
+    if (j == 1) { // check the next column
+      if (nextSquares[i][j + 2] == null) { // if the rightmost square is null, it moves there
+        return j + 2;
+      }
+      if (nextSquares[i][j + 1] == null) { // if the next square is null, it moves there
+        return j + 1;
+      }
+      else { // otherwise, it does not move
+        return j;
+      }
+    }
+    if (j == 0) { // check the next row 
+      if (nextSquares[i][j + 3] == null) { // if the rightmost square is null, it moves there
+        return j + 3;
+      }
+      if (nextSquares[i][j + 2] == null) { // if the next square is null, it moves there
+        return j + 2;
+      }
+      if (nextSquares[i][j + 1] == null) { // if the next square is null, it moves there
+        return j + 1;
+      }
+      else { // otherwise, it does not move
+        return j;
+      }
+    }
+  }
   return null;
 }
 
